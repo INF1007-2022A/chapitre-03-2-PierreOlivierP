85d82315d363c0b2041ff57f47b1c4af1e03d510
PierreOlivierP <pierre-olivier.piquant@polymtl.ca>
2022-10-14 00:06:40 -0400

    zoom

--- exercice.py
+++ exercice.py
@@ -3,29 +3,54 @@
 
 def dissipated_power(voltage, resistance):
 	# TODO: Calculer la puissance dissipée par la résistance.
-	return 0
+	Power = voltage**2 / resistance
+	return Power
 
 def orthogonal(v1, v2):
 	# TODO: Retourner vrai si les vecteurs sont orthogonaux, faux sinon.
-	v1[0] # Pour accéder au X
-	v1[1] # Pour accéder au Y
+	#v1[0] # Pour accéder au X
+	#v1[1] # Pour accéder au Y
+	if v1[0] * v2[0] + v2[1] * v1[1] == 0:
+		return True
+	else:
+		return False
 	pass
 
 def average(values):
 	# TODO: Calculer la moyenne des valeurs positives (on ignore les valeurs strictement négatives).
+	a = 0
+	n = 0
 	for v in values:
-		pass # La variable v contient une valeur de la liste.
+		if v > 0:
+			a = a+v
+			n +=1
+	moyenne = a/n
+	return moyenne
+	pass # La variable v contient une valeur de la liste.
 
 def bills(value):
 	# TODO: Calculez le nombre de billets de 20$, 10$ et 5$ et pièces de 1$ à remettre pour représenter la valeur.
+	twenties = 0
+	tens = 0
+	fives = 0
+	twos = 0
+	ones = 0
 	while value != 0:
 		if value >= 20:
+			value = value-20
+			twenties +=1
 			pass
 		elif value >= 10:
+			value = value-10
+			tens +=1
 			pass
 		elif value >= 5:
+			value = value-5
+			fives +=1
 			pass
 		elif value >= 1:
+			value = value-1
+			ones +=1
 			pass
 
 	return (twenties, tens, fives, twos, ones);
@@ -35,9 +60,29 @@ def format_base(value, base, digit_letters):
 	# `digits_letters[0]` Nous donne la lettre pour le chiffre 0, ainsi de suite.
 	result = ""
 	abs_value = abs(value)
+	max = 0
+	i = 0
+	basepui = 0
+	while basepui < abs_value:
+		basepui = base
+		max += 1
+		i += 1
+		basepui = base ** i
+	i-=1
+	max-=1
 	while abs_value != 0:
+		fois = int(abs_value/(base**max))
+		chiffre = digit_letters[fois]
+		result = result + chiffre
+		abs_value = abs_value-((base**max)*fois)
+		max -=1
+
+
 		pass
+
+
 	if value < 0:
+		result = '-' + result
 		# TODO: Ne pas oublier d'ajouter '-' devant pour les nombres négatifs.
 		pass
 	return result
@@ -49,3 +94,4 @@ if __name__ == "__main__":
 	print(average([1, 4, -2, 10]))
 	print(bills(137))
 	print(format_base(-42, 16, "0123456789ABCDEF"))
+
